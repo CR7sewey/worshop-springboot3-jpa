cf6ef397fe62cd669cd0f26977b943894339d68b
CR7sewey <miguelmotacarvalho@gmail.com>
2023-10-25 10:42:01 +0100

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,10 +3,20 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // depender da especificaçao e nao da implementacao
+@Table(name = "tb_user")
 public class User implements Serializable { // transofmrar em cadeia de bytes para arquivos e tal
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto incrment
 	private Long id;
 	private String name;
 	private String email;
