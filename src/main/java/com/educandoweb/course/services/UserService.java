34f29b917c0d9355cde8b2423d8245a212f60444
CR7sewey <miguelmotacarvalho@gmail.com>
2023-10-26 10:56:31 +0100

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 // registar a classe como componente do Spring
 @Service
 public class UserService {
@@ -27,7 +29,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // retonra um user dentro do opcional
-	// tenta get, se nao der retorna excecao
+		// tenta get, se nao der retorna excecao
 	}
 
 	public User insert(User obj) {
@@ -38,24 +40,29 @@ public class UserService {
 
 	public void delete(Long id) {
 		try {
-		repository.deleteById(id); // ja salva, do repository
-		}
-		catch (EmptyResultDataAccessException e) {  // delete something that isnt there (an id)
+			repository.deleteById(id); // ja salva, do repository
+		} catch (EmptyResultDataAccessException e) { // delete something that isnt there (an id)
 			throw new ResourceNotFoundException(id);
-		}
-		catch (DataIntegrityViolationException e) {  // catch when delete something that as orders!
+		} catch (DataIntegrityViolationException e) { // catch when delete something that as orders!
 			throw new DatabaseException(e.getMessage());
 		}
 
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id); // instancia o usuario sem ir ao banco de dados, monitorado pelo jpa!!
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id); // instancia o usuario sem ir ao banco de dados, monitorado
+															// pelo jpa!!
+			updateData(entity, obj);
+			return repository.save(entity);	
+			// fazer catch com RuntimeException e e.print.. para ver a excecao lancada e tratar
+		} catch (EntityNotFoundException e) { // catch when delete something that as orders!
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
-	
-	private void updateData(User entity,User obj) {
+
+	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone()); // nao atualizo id nem semnha
